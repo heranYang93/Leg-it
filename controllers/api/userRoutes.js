1a90de1a250b676515de8d557a850ecb9ad651f4
Real Inspiration <ilerwatson22@gmail.com>
2022-02-17 13:39:57 +0000

    Added functionality to create a user and find a user by id which can be amended to find one.

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const User = require('../../models/User');
+
+// Get one user
+router.get('/:id', async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.params.id);
+    if (!userData) {
+      res.status(404).json({ message: 'No user with this id!' });
+      return;
+    }
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// CREATE a new user
+router.post('/', async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
