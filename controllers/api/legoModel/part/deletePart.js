7914eecc1399c5b0e037eca85e8dc08f30ad998a
hy <yang.hrn@gmail.com>
2022-02-19 02:31:29 +0000

    part get, get associate, delete

--- controllers/api/legoModel/part/deletePart.js
+++ controllers/api/legoModel/part/deletePart.js
@@ -1,6 +1,22 @@
 const router = require('express').Router();
+const { Part, LegoPart } = require('../../../../models');
+const withAuth = require('../../../../utils/auth');
 
-// const createLego = require('./createLego');
-// router.use('/create', createLego);
+router.delete('/:id', async (req, res) => {
+  try {
+    //update lego model itself and get the lego model id
+    const deletePartModel = await Part.destroy({
+      where: { id: req.params.id },
+    });
+
+    const deleteAssociation = LegoPart.destroy({
+      where: { part_id: req.params.id },
+    });
+    console.log('in delete part route');
+    res.status(200).json(deleteAssociation).json(deletePartModel);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 module.exports = router;
--- controllers/api/legoModel/part/getPartWithLegos.js
+++ controllers/api/legoModel/part/getPartWithLegos.js
@@ -1,6 +1,23 @@
 const router = require('express').Router();
+const { Lego, Part } = require('../../../../models');
+const pageToRender = 'lego';
 
-// const createLego = require('./createLego');
-// router.use('/create', createLego);
+//Authentication method tbd
+// const withAuth = require('../../../../utils/auth');
+
+router.get('/:id', async (req, res) => {
+  try {
+    //update lego model itself and get the lego model id
+    const getPartModelWithLegos = await Part.findByPk(req.params.id, {
+      include: [{ model: Lego }],
+    });
+
+    const partModelWithLegos = getPartModelWithLegos.get({ plain: true });
+
+    res.status(200).json(partModelWithLegos);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 module.exports = router;
