6eb8fc3d4a64fe5d18f7d4b5def2aa0cf43b6c23
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2022-02-23 15:26:44 +0000

    added favourites page with functioning like feature

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -120,6 +120,45 @@ router.get('/feed', async (req, res) => {
   }
 });
 
+router.get('/favourites', async (req, res) => {
+  try {
+    const dbpostsData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: {
+            exclude: ['password', 'email'],
+          },
+        },
+        {
+          model: Like,
+        },
+        {
+          model: Comment,
+        },
+      ],
+      order: [['updatedAt', 'DESC']],
+    });
+    const postsData = dbpostsData.map((el) => el.get({ plain: true }));
+    postsData.map(
+      (e) =>
+        (e.like =
+          e.likes.filter((e) => e.user_id === req.session.user.id).length > 0)
+    );
+    const filteredData = postsData.filter((e) => e.like === true);
+    console.log(filteredData);
+    res.render('feed', {
+      title: 'Lego Posts',
+      postsData: filteredData,
+      signedIn: req.session.logged_in,
+      loggedOut: !req.session.logged_in,
+      user: req.session.user_name,
+    });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
+});
+
 router.get('/login', async (req, res) => {
   try {
     res.render('login');
