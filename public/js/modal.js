97c797ecacf07ce952c4867cb2d2fcfb36dc9731
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2022-02-27 10:54:58 +0000

    adds ui for image upload to all pages

--- public/js/modal.js
+++ public/js/modal.js
@@ -1,5 +1,5 @@
-const actionsModal = document.querySelector('.actions-modal');
-const modalTrigger = document.querySelector('.modal-trigger');
+const actionsModal = document.querySelector('.actions-modal.actions');
+const modalTrigger = document.querySelector('.modal-trigger.actions');
 
 // This adds an event listener on h3 headers to fetch repo data
 
@@ -10,13 +10,15 @@ modalTrigger.addEventListener('click', function () {
   try {
     actionsModal.classList.add('is-active'); // modal status to active to enable the following event listener to exit modal screen on modal-background click
 
-    const modalBg = document.querySelector('.modal-background');
+    const modalBg = document.querySelector('.modal-background.actions');
     modalBg.addEventListener('click', function () {
       actionsModal.classList.remove('is-active');
     });
-    document.querySelector('.cancel').addEventListener('click', function () {
-      actionsModal.classList.remove('is-active');
-    });
+    document
+      .querySelector('.cancel.actions')
+      .addEventListener('click', function () {
+        actionsModal.classList.remove('is-active');
+      });
   } catch (error) {
     console.error(error);
   }
