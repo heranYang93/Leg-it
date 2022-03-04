57bd73007112cef7f4458342a16a3b687d51aa0e
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2022-03-04 03:16:07 +0000

    enables follow feature from userPage

---
+++ public/js/followUser.js
@@ -0,0 +1,16 @@
+import { makeRequest } from './helpers.js';
+
+const followUserBtn = document.querySelector('.userfollow-btn');
+
+followUserBtn.addEventListener('click', async (event) => {
+  event.preventDefault();
+  //   console.log(element.innerHTML);
+  const id = followUserBtn.getAttribute('user-id');
+  const followedBy = document.querySelector('.followers');
+  try {
+    const data = await makeRequest(`/api/follow/${id}`, 'POST');
+    window.location.reload();
+  } catch (error) {
+    console.log('Failed to login', error);
+  }
+});
