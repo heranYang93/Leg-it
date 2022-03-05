331f37c916bca86e0ba40aafc9804476c1a23988
hy <yang.hrn@gmail.com>
2022-03-05 04:12:41 +0000

    add a helper to size the images

--- utils/helpers.js
+++ utils/helpers.js
@@ -34,34 +34,36 @@ module.exports = {
     return options.inverse(this);
   },
 
-
-// a limited 'each' loop.
-// usage: {{#limit items offset="1" limit="5"}} : items 1 thru 6
-// usage: {{#limit items limit="10"}} : items 0 thru 9
-// usage: {{#limit items offset="3"}} : items 3 thru context.length
-// defaults are offset=0, limit=5
-limit: function(context, block) {
-  var ret = "",
+  // a limited 'each' loop.
+  // usage: {{#limit items offset="1" limit="5"}} : items 1 thru 6
+  // usage: {{#limit items limit="10"}} : items 0 thru 9
+  // usage: {{#limit items offset="3"}} : items 3 thru context.length
+  // defaults are offset=0, limit=5
+  limit: function (context, block) {
+    var ret = '',
       offset = parseInt(block.hash.offset) || 0,
       limit = parseInt(block.hash.limit) || 5,
-      i = (offset < context.length) ? offset : 0,
-      j = ((limit + offset) < context.length) ? (limit + offset) : context.length;
+      i = offset < context.length ? offset : 0,
+      j = limit + offset < context.length ? limit + offset : context.length;
 
-  for(i,j; i<j; i++) {
-    ret += block(context[i]);
-  }
+    for (i, j; i < j; i++) {
+      ret += block(context[i]);
+    }
 
-  return ret;
-},
+    return ret;
+  },
 
-limitEach: function(ary, max, options) {
-  if(!ary || ary.length == 0)
-      return options.inverse(this);
+  limitEach: function (ary, max, options) {
+    if (!ary || ary.length == 0) return options.inverse(this);
 
-  var result = [ ];
-  for(var i = 0; i < max && i < ary.length; ++i)
+    var result = [];
+    for (var i = 0; i < max && i < ary.length; ++i)
       result.push(options.fn(ary[i]));
-  return result.join('');
-},
+    return result.join('');
+  },
 
+  urlCompiler: function (str, query) {
+    return (recompiled =
+      str.split(/upload/)[0] + `upload/${query}` + str.split(/upload/)[1]);
+  },
 };
